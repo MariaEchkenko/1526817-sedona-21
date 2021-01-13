d5c692f23a792d14ca2f887ae1fcab38752b1481
Maria <mashasly@mail.ru>
2021-01-14 01:48:20 +0300

    Внесены правки согласно критериям

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 var buttonForm = document.querySelector(".review-form__button");
 var errorPopup = document.querySelector(".modal--error");
 var successPopup = document.querySelector(".modal--success");
@@ -7,15 +7,15 @@ var successPopup = document.querySelector(".modal--success");
 
 // Mobile menu
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
 
